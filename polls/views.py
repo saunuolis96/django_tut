3bd9fb983f5ab148fc52adfd9f71a490f15a2865
saunuolis96 <meivaidas@gmail.com>
2018-01-13 23:18:41 +0000

    before 4th django transformation to djangos general views

--- polls/views.py
+++ polls/views.py
@@ -1,32 +1,14 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect, HttpResponse
-# from django.template import loader
 from .models import Choice, Question
 from django.urls import reverse
-from django.http import Http404
 
-
-# def index(request):
-#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-#     # output = ', '.join([q.question_text for q in latest_question_list])
-#     template = loader.get_template('polls/index.html')
-#     context = {
-#         'latest_question_list': latest_question_list,
-#     }
-#     # return HttpResponse(output)
-#     return HttpResponse(template.render(context, request))
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     context = {'latest_question_list': latest_question_list}
     return render(request, 'polls/index.html', context)
 
-# def detail(request, question_id):
-#     try:
-#         question = Question.objects.get(pk=question_id)
-#     except Question.DoesNotExist:
-#         raise Http404("Question does not exits")
-#     return render(request, 'polls/detail.html', {'question': question} )
 
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
@@ -36,8 +18,6 @@ def detail(request, question_id):
 def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/results.html', {'question':question})
-    # response = "You're looking at the results of question %s."
-    # return HttpResponse(response % question_id)
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
